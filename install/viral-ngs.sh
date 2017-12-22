4e32e970c42e0aff8bc993c92a4729691ccd79fb
David J. H. Shih <djh.shih@gmail.com>
2017-12-22 18:45:17 -0500

    Added installation scripts for viral-ngs.

---
+++ install/viral-ngs.sh
@@ -0,0 +1,8 @@
+# viral-ngs.readthedocs.io
+
+conda update -y conda
+conda config --add channels r
+conda config --add channels defaults
+conda config --add channels conda-forge
+conda config --add channels bioconda
+conda config --add channels broad-viral
